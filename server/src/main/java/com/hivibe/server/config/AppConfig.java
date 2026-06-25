0bb08bd484dc516197901cda2d26268bdca62439
three-co1ors <jelly__sh@naver.com>
2026-06-25 17:01:11 +0900

    [FIX] 마이페이지, 노트 에러 해결
    AppConfig.java 파일 수정

--- server/src/main/java/com/hivibe/server/config/AppConfig.java
+++ server/src/main/java/com/hivibe/server/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.hivibe.server.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +17,11 @@ public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        // LocalDateTime 등 java.time 타입을 직렬화할 수 있게 모듈 등록
+        objectMapper.registerModule(new JavaTimeModule());
+        // 날짜를 숫자 타임스탬프 배열이 아니라 ISO 문자열로 직렬화
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
 }
